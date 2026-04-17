3fcdabc4ac363e67ef270ddc24dd5b84582929ad
lafapuffs <rafaelmoisesvalera@gmail.com>
2026-04-17 11:31:38 -0400

    Agregando manipulacion de string

--- TallerPractico01/TallerPractico01/Program.cs
+++ TallerPractico01/TallerPractico01/Program.cs
@@ -1,12 +1,4 @@
-﻿/*
- * Creado por SharpDevelop.
- * Usuario: Usuario
- * Fecha: 17/4/2026
- * Hora: 10:53 a. m.
- * 
- * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
- */
-using System;
+﻿using System;
 
 namespace TallerPractico01
 {
@@ -14,9 +6,24 @@ namespace TallerPractico01
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("===Taller 01===");
 			
-			// TODO: Implement Functionality Here
+			// 1. El dato del usuario
+			string registroUsuario = "         ID_1314; Pedro Sanchez; Evaluacion_Guiada; 95";
+			
+			Console.WriteLine(registroUsuario);
+			
+			string registroLimpio = registroUsuario.Trim();
+			Console.WriteLine(registroLimpio);
+			
+			string[] partes = registroLimpio.Split(';');
+			
+			string id = partes[0].Trim();
+			string nombre = partes[1].Trim();
+			string tarea = partes[2].Trim();
+			string nota = partes[3].Trim();
+			
+			Console.WriteLine(string.Format("El ID es: {0} del usuario {1}. Tarea: {2} con la nota total de: {3}", id, nombre, tarea, nota));
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
